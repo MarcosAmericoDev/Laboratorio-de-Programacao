aaab16bf5920956fd11ae463b526557b08e468b7
Marcos Iury de Lima Américo <MarcosIuryProgramador@gmail.com>
2023-04-17 17:55:48 -0300

    Adicionando a primeira questao da Lista 4

---
+++ Lista4/01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    /* Escreva um programa que adicione dois números usando ponteiros. Além do valor da soma, imprima
+    também o endereço de memória onde o valor resultante dessa soma está armazenado. */ 
+    
+    // Entrada
+    int n1, n2, soma;
+    int *pN1 = &n1, *pN2 = &n2, *pSoma = &soma;
+
+    puts("Digite o primeiro valor: ");
+    scanf("%d", pN1);
+    puts("Digite o segundo valor: ");
+    scanf("%d", pN2);
+    
+    // Processamento
+    *pSoma = *pN1 + *pN2;
+
+    // Saída
+
+    printf("[%p] soma: %d", pSoma, *pSoma);
+
+    return 0;
+}
