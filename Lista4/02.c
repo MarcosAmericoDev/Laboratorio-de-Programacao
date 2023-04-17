8a3f10514948af672fc82b0c4212889b487d99e1
Marcos Iury de Lima Américo <MarcosIuryProgramador@gmail.com>
2023-04-17 18:09:49 -0300

    Adicionando a segunda questao da lista 4

---
+++ Lista4/02.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void imprimirPonteiros(int *p1, int *p2);
+int main(void) {
+
+    /* Escreva um programa que troque o valor de dois números utilizando ponteiros. */
+
+    // Entrada
+
+    int n1, n2;
+    int *pN1 = &n1, *pN2 = &n2;
+
+    puts("Digite o primeiro valor:");
+    scanf("%d", pN1);
+    puts("Digite o segundo valor:");
+    scanf("%d", pN2);
+
+    // Processamento
+    puts("=========Antes da troca=======");
+    imprimirPonteiros(pN1, pN2);
+
+    *pN1 = *pN1 ^ *pN2;
+    *pN2 = *pN1 ^ *pN2;
+    *pN1 = *pN1 ^ *pN2;
+
+    // Saída
+    puts("========Depois da troca======");
+    imprimirPonteiros(pN1, pN2);
+    return 0;
+}
+
+void imprimirPonteiros(int *p1, int *p2){
+    printf("[%p]: %d \n[%p]: %d\n", p1, *p1, p2, *p2);
+}
