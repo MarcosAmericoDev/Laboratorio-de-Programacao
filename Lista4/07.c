5447c69eb7069f5126d68e575c3b8c4d8bbb626f
Marcos Iury de Lima Américo <MarcosIuryProgramador@gmail.com>
2023-05-03 19:46:38 -0300

    Adicionando a questao 7

---
+++ Lista4/07.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#define TAM 40
+
+int main(void) {
+
+    char s1[TAM], s2[TAM], *pS1 = s1, *pS2 = s2;
+
+    // Entrada
+
+    puts("Digite as suas strings: ");
+    fgets(s1, TAM, stdin);
+    *(pS1 + strlen(pS1) - 1) = '\0';
+    fflush(stdin);
+    fgets(s2, TAM, stdin);
+
+    // Processamento
+    strcat(pS1, pS2);
+
+    // Saída
+
+    puts(s1);
+
+    return 0;
+}
