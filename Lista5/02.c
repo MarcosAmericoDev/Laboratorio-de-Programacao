c4239eff913c1c28a9da7d3a84cb1adfbdc67306
Marcos Iury de Lima Américo <MarcosIuryProgramador@gmail.com>
2023-05-29 13:11:46 -0300

    revertendo

--- Lista5/02.c
+++ Lista5/02.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 // Escreva um programa em C que informe o endereço do menor elemento de um vetor de inteiros
 // gerado. A quantidade de elementos desse array deve ser fornecido por linha de comando.
